a91130fcd2d5ac72ebb4bb384765099a5f240734
kvebers <kvebers@student@42heilbronn.de>
2024-10-27 14:40:00 +0100

    Add changes to facilitate work with api

--- react/src/components/movieDetails.jsx
+++ react/src/components/movieDetails.jsx
@@ -1,31 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "../App.css";
 
 const MovieDetails = () => {
-  //   const [query, setQuery] = useState("");
-  //   const [results, setResults] = useState([]);
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/api/watchTheMovie?id=${id}`
+        );
+        if (!response.ok) throw new Error("Failed to fetch movie details");
 
-  //   const handleApiRequest = async (e) => {
-  //     const value = e.target.value;
-  //     setQuery(value);
-  //     if (value) {
-  //       try {
-  //         const response = await fetch(
-  //           `http://localhost:3000/api/movies?title=${value}`
-  //         );
-  //         const data = await response.json();
-  //         setResults(data.Search || []);
-  //       } catch (error) {
-  //         console.error("Error fetching data:", error);
-  //       }
-  //     } else {
-  //       setResults([]);
-  //     }
-  //   };
+        const data = await response.json();
+        setMovie(data);
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [id]);
+
+  if (loading) return <p>Loading...</p>;
 
   return (
     <div className="center">
       <h1>HyperTube</h1>
+      {movie ? (
+        <div className="movie-details">
+          <img src={movie.Poster} alt={movie.Title} />
+          <h2>{movie.Title}</h2>
+          <p>Year: {movie.Year}</p>
+          <p>Genre: {movie.Genre}</p>
+          <p>Plot: {movie.Plot}</p>
+          <p>Director: {movie.Director}</p>
+        </div>
+      ) : (
+        <p>Movie details not found.</p>
+      )}
     </div>
   );
 };
