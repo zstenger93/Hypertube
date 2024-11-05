f44716d71958b18bc5ffd7fc8e494e4b76adcfc6
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2024-11-05 12:11:47 +0100

    Bug fix

--- node/index.js
+++ node/index.js
@@ -59,8 +59,8 @@ app.get("/api/watchTheMovie", async (req, res) => {
 
 app.get("/api/youtubeRequests", async (req, res) => {
   const { title } = req.query;
-  const movie = "_movie";
-  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3 &q=${title}${movie}&key=${youtubeApiKey}`;
+  const movie = "%movie";
+  const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${title}&key=${youtubeApiKey}`;
   try {
     const response = await axios.get(url);
     res.json(response.data);
