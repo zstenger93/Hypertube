4a6081e97922c31fe185cc6a9923bc314c03602f
Zsolt Stenger <zstenger@student.42heilbronn.de>
2025-02-22 12:15:54 +0100

    misc: made the search page a bit more piraty xD

--- react/src/components/searchMovies.jsx
+++ react/src/components/searchMovies.jsx
@@ -83,7 +83,11 @@ const SearchComponent = () => {
     <div className="center">
       <div className="searchBox">
         <Logout />
-        <h1 className="lastColor">HyperCrime</h1>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+			<img src="Pirate-Flag.svg" alt="Left SVG" style={{ width: '148px', height: '148px', marginRight: '10px' }} />
+			<h1 className="lastColor">HyperCrime</h1>
+			<img src="Pirate-Flag.svg" alt="Right SVG" style={{ width: '148px', height: '148px', marginLeft: '10px' }} />
+      	</div>
         <input
           type="text"
           placeholder="Search..."
