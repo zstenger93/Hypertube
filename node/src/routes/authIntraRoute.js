ac421e35987d85006807cdbc235e72237b1c18b1
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2025-08-20 12:34:29 +0200

    Minor fix in the env, how the ip is loaded for auth.

--- node/src/routes/authIntraRoute.js
+++ node/src/routes/authIntraRoute.js
@@ -3,7 +3,7 @@ import { client } from "../../index.js";
 import axios from "axios";
 const router = express.Router();
 const intraUUID = process.env.INTRA_UUID;
-const redirectURI = `http://${process.env.REACT_APP_IP}/auth/intra/callback`;
+const redirectURI = `http://${process.env.IP}/auth/intra/callback`;
 
 router.get("/", (req, res) => {
   const authURL = `https://api.intra.42.fr/oauth/authorize?client_id=${intraUUID}&redirect_uri=${redirectURI}&response_type=code`;
